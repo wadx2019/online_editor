ba4cc0612ea5794477de9ba7365847910317cbf2
wadx2019 <48609729+wadx2019@users.noreply.github.com>
2019-12-11 03:01:59 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -65,132 +65,162 @@ def register():
 def createProject():
     global dbop
     response={"state":0,'pno':None}
-    if session.get('name_psd') in name_psd:
-        pname=request.form.get('pname')
-        pno=generatorPno(pname)
-        key={'pno':pno}
-        while dbop.select_by_key('PROJECTS',key):
-            pno=str((int(pno)+1)%int(1e8)).zfill(8)
-            key['pno']=pno
-        key={'pno':pno,'pname':pname,'createtime':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),'pmaster':session['name_psd'][0]}
-        attribute=[['fname','varchar(256)','primary key'],['fcontent','longtext'],['createtime','datetime'],['currentedit','varchar(256)']]
-        if dbop.insert_by_key('PROJECTS',key) and dbop.insert_by_table(pname,attribute):
-            response['state']=1
-            response['pno']=pno
-    else:
-        response['state']=2
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            pname=request.form.get('pname')
+            pno=generatorPno(pname)
+            key={'pno':pno}
+            while dbop.select_by_key('PROJECTS',key):
+                pno=str((int(pno)+1)%int(1e8)).zfill(8)
+                key['pno']=pno
+            key={'pno':pno,'pname':pname,'createtime':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),'pmaster':session['name_psd'][0]}
+            attribute=[['fname','varchar(128)','primary key'],['fcontent','longtext'],['createtime','datetime'],['currentedit','varchar(128)']]
+            if dbop.insert_by_key('PROJECTS',key) and dbop.insert_by_table(pname,attribute):
+                response['state']=1
+                response['pno']=pno
+        else:
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/createFile',methods=['POST'])
 def createFile():
     global dbop
     response={"state":0}
-    if session.get('name_psd') in name_psd:
-        pname=request.form.get('pname')
-        fname=request.form.get('fname')
-        key={'fname':fname}
-        if not dbop.select_by_key(pname,key):
-            key['createtime']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            if dbop.insert_by_key(pname,key):
-                response['state']=1
-    else:
-        response['state']=2
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            pname=request.form.get('pname')
+            fname=request.form.get('fname')
+            key={'fname':fname}
+            if not dbop.select_by_key(pname,key):
+                key['createtime']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                if dbop.insert_by_key(pname,key):
+                    response['state']=1
+        else:
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/modifyFile',methods=['POST'])
 def modifyFile():
     global dbop
     response={"state":0}
-    if session.get('name_psd') in name_psd:
-        pname=request.form.get('pname')
-        fname=request.form.get('fname')
-        fcontent=request.form.get('fcontent')
-        currentedit=session.get('name_psd')[0]
-        key={'fname':fname}
-        f=dbop.select_by_key(pname,key)
-        print(f)
-        if not f[0]['currentedit'] or f[0]['currentedit']==session.get('name_psd')[0]:
-            value={'fcontent':fcontent,'currentedit':currentedit}
-            if dbop.update(pname,key,value):
-                pf_editor[(pname,fname,currentedit)]=time.time()
-                response['state']=1
-    else:
-        response['state']=2
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            pname=request.form.get('pname')
+            fname=request.form.get('fname')
+            fcontent=request.form.get('fcontent')
+            currentedit=session.get('name_psd')[0]
+            key={'fname':fname}
+            f=dbop.select_by_key(pname,key)
+            if not f[0]['currentedit'] or f[0]['currentedit']==session.get('name_psd')[0]:
+                value={'fcontent':fcontent,'currentedit':currentedit}
+                if dbop.update(pname,key,value):
+                    pf_editor[(pname,fname,currentedit)]=time.time()
+                    response['state']=1
+        else:
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/query',methods=['POST'])
 def query():
     global dbop
     response={"state":0}
-    if session.get('name_psd') in name_psd:
-        op=request.form.get('op')
-        if op=='pro':
-            pno=request.form.get('pno')
-            key={'pno':pno}
-            project=dbop.select_by_key('PROJECTS',key)[0]
-            project['CREATETIME']=project['CREATETIME'].strftime("%Y-%m-%d %H:%M:%S")
-            if project:
-                pname=project['PNAME']
-                files=dbop.select_by_table(pname)
-                for i in range(len(files)):
-                    files[i]={'fname':files[i]['fname'],'createtime':files[i]['createtime'].strftime("%Y-%m-%d %H:%M:%S"),'currentedit':files[i]['currentedit']}
-                response['poject']=project
-                response['files']=files
-                response['state']=1
-        elif op=='file':
-            pname=request.form.get('pname')
-            fname=request.form.get('fname')
-            key={'fname':fname}
-            data=dbop.select_by_key(pname,key)
-            if data:
-                file=data[0]
-                file['createtime']=file['createtime'].strftime("%Y-%m-%d %H:%M:%S")
-                response['file']=file
-                response['state']=1
-    else:
-        response['state']=2
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            op=request.form.get('op')
+            if op=='pro':
+                pno=request.form.get('pno')
+                key={'pno':pno}
+                project=dbop.select_by_key('PROJECTS',key)[0]
+                project['CREATETIME']=project['CREATETIME'].strftime("%Y-%m-%d %H:%M:%S")
+                if project:
+                    pname=project['PNAME']
+                    files=dbop.select_by_table(pname)
+                    if files:
+                        for i in range(len(files)):
+                            files[i]={'fname':files[i]['fname'],'createtime':files[i]['createtime'].strftime("%Y-%m-%d %H:%M:%S"),'currentedit':files[i]['currentedit']}
+                    response['poject']=project
+                    response['files']=files
+                    response['state']=1
+            elif op=='file':
+                pname=request.form.get('pname')
+                fname=request.form.get('fname')
+                key={'fname':fname}
+                data=dbop.select_by_key(pname,key)
+                if data:
+                    file=data[0]
+                    file['createtime']=file['createtime'].strftime("%Y-%m-%d %H:%M:%S")
+                    response['file']=file
+                    response['state']=1
+        else:
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/removeProject',methods=['POST'])
 def removeProject():
     global dbop
     response={"state":0}
-    if session.get('name_psd') in name_psd:
-        pname=request.form.get('pname')
-        key={'pname':pname}
-        tmp=dbop.select_by_key('PROJECTS',key)
-        if tmp[0]['PMASTER']==session.get("name_psd")[0]:
-            if dbop.delete_by_table(pname) and dbop.delete_by_key('PROJECTS',key):
-                response['state']=1
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            pname=request.form.get('pname')
+            key={'pname':pname}
+            tmp=dbop.select_by_key('PROJECTS',key)
+            if tmp[0]['PMASTER']==session.get("name_psd")[0]:
+                if dbop.delete_by_table(pname) and dbop.delete_by_key('PROJECTS',key):
+                    response['state']=1
+            else:
+                response['state']=3
         else:
-            response['state']=3
-    else:
-        response['state']=2
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/removeFile',methods=['POST'])
 def removeFile():
     global dbop
     response={"state":0}
-    if session.get('name_psd') in name_psd:
-        pname=request.form.get('pname')
-        key={'fname':request.form.get('fname')}
-        if not dbop.select_by_key(pname,key)[0]['currentedit'] and dbop.delete_by_key(pname,key):
-            response['state']=1
-    else:
-        response['state']=2
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            pname=request.form.get('pname')
+            key={'fname':request.form.get('fname')}
+            if not dbop.select_by_key(pname,key)[0]['currentedit'] and dbop.delete_by_key(pname,key):
+                response['state']=1
+        else:
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/finishFile',methods=['POST'])
 def finishFile():
     global dbop
     response={'state':0}
-    if session.get('name_psd') in name_psd:
-        fname=request.form.get('fname')
-        pname=request.form.get('pname')
-        editor=session.get('name_psd')[0]
-        key={'fname':fname,'currentedit':editor}
-        value={'currentedit':None}
-        if dbop.update(pname,key,value):
-            try:
-                pf_editor.pop(tuple([pname])+tuple(key.values()))
-            except:
-                pass
-            response['state']=1
+    try:
+        if session.get('name_psd') in name_psd:
+            name_psd[session.get('name_psd')]=time.time()
+            fname=request.form.get('fname')
+            pname=request.form.get('pname')
+            editor=session.get('name_psd')[0]
+            key={'fname':fname,'currentedit':editor}
+            value={'currentedit':None}
+            if dbop.update(pname,key,value):
+                try:
+                    pf_editor.pop(tuple([pname])+tuple(key.values()))
+                except:
+                    pass
+                response['state']=1
+        else:
+            response['state']=2
+    except Exception as e:
+        print(e)
     return json.dumps(response)
 @app.route('/logout',methods=['POST'])
 def logout():
